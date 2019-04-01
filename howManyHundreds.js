27ca4dc642efeab988e61897c9238fb1d8241032
Annie <anniekao@posteo.net>
2019-04-01 20:07:42 +0200

    how many hundreds assignment with howManyHundreds function completed

---
+++ howManyHundreds.js
@@ -0,0 +1,9 @@
+function howManyHundreds(num){
+  return Math.trunc(num / 100);
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
