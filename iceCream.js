06e7afeed27930234a99bbc82b72d7615ad7008b
Annie <anniekao@posteo.net>
2019-04-02 15:38:43 +0200

    ice cream exercise with completed array tasks

---
+++ iceCream.js
@@ -0,0 +1,10 @@
+var iceCreamFlavours = ["chocolate", "vanilla", "cookes and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]);
+
+console.log(iceCreamFlavours[iceCreamFlavours.length-1]);
+
