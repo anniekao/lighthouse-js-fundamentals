16b0cfdc2a851c3a2ff1a5458f6a07fbbc851e27
Annie <anniekao@posteo.net>
2019-04-02 15:37:41 +0200

    Adds conditional exercises

---
+++ go-outside.js
@@ -0,0 +1,41 @@
+/* var raining = true;
+
+if (raining) {
+    console.log("Don't forget your umbrella!");
+} */
+
+
+/* var raining = true;
+var cold = false;
+
+if (raining) {
+    console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+    console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!"); */
+
+
+/* var cold = false;
+
+if (cold) {
+    console.log("Make sure you pick out a scarf!");
+} else {
+    console.log("Short sleeves are fine.");
+} */
+
+
+var temperature = 20;
+
+if (temperature < 0) {
+    console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+    console.log("Short sleeves won't cut it!");
+} else {
+    console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside");
