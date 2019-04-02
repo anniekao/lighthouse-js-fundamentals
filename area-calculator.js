032709742725492096e414123025fe474de90173
Annie <anniekao@posteo.net>
2019-04-03 00:23:02 +0200

    Deleted console.log tests

--- area-calculator.js
+++ area-calculator.js
@@ -21,12 +21,3 @@ function calculateCircleArea(radius) {
          return Math.PI * Math.pow(radius, 2);
      }
 }
-
-console.log(calculateTriangleArea(10, 5)); // should print 25
-console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
-console.log(calculateTriangleArea(10, -5)); // should print undefined
-
-console.log(calculateCircleArea(10)); // should print 314.159...
-console.log(calculateCircleArea(3.5)); // should print 38.484...
-console.log(calculateCircleArea(-1)); // should print undefined
-
