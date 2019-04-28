0aa32f8357fadcfc8dac14a384828dcc5cdb8dcd
Annie <anniekao@posteo.net>
2019-04-28 22:26:09 +0200

    Added kata exercise by completing the function repeatNumbers

---
+++ katas/kata6-repeating-numbers.js
@@ -0,0 +1,22 @@
+let repeatNumbers = function (data) {
+    var repeatStr = "";
+    for (var i = 0; i < data.length; i++){
+        var toStr = data[i][0].toString();
+        repeatStr += toStr.repeat(data[i][1]) + ", "; 
+    }
+    return repeatStr.substring(0, repeatStr.length-2);
+};
+
+console.log(repeatNumbers([
+    [1, 10]
+]));
+console.log(repeatNumbers([
+    [1, 2],
+    [2, 3]
+]));
+console.log(repeatNumbers([
+    [10, 4],
+    [34, 6],
+    [92, 2]
+]));
+
