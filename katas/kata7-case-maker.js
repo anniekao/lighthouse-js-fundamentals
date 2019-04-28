2d62b3b9f6bdb2bf6f9adfbd61b241059c3ca265
Annie <anniekao@posteo.net>
2019-04-28 23:38:46 +0200

    Added case-maker.js with working camelCase function

---
+++ katas/kata7-case-maker.js
@@ -0,0 +1,16 @@
+let camelCase = function (input) {
+    var camelCased = "";
+
+    for (var i = 0; i < input.length; i += 1){
+        if (input[i-1] == " "){
+            camelCased += input[i].toUpperCase();
+        } else if (input[i] != " ") {
+            camelCased += input[i];
+        }
+    }
+    return camelCased;
+};
+
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
