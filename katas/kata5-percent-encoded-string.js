6af4adc323bd3b03e2334056af64ed199f97dade
Annie <anniekao@posteo.net>
2019-04-28 17:43:50 +0200

    Added kata exercise Percent Encoded String with completed function urlEncode

---
+++ katas/kata5-percent-encoded-string.js
@@ -0,0 +1,16 @@
+const urlEncode = function (text) {
+    var encodedStr = "";
+
+    for (var i = 0; i < text.length; i ++){
+        if (text[i] == " " && !i == 0){
+            encodedStr += "%20";
+        } else {
+            encodedStr += text[i];
+        }
+    }
+    return encodedStr;
+};
+
+console.log(urlEncode("Lighthouse Labs"));
+console.log(urlEncode(" Lighthouse Labs "));
+console.log(urlEncode("blue is greener than purple for sure"));
