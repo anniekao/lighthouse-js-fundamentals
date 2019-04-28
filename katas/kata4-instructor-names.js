b5d8ce1463ac434821b52e6edce059c5c3999472
Annie <anniekao@posteo.net>
2019-04-28 17:36:16 +0200

    Added kata exercise Instructor Names with completed function instructorWithLongestName

---
+++ katas/kata4-instructor-names.js
@@ -0,0 +1,28 @@
+const instructorWithLongestName = function (instructors) {
+    var longestName = "";
+    var result;
+    
+    for (var i = 0; i < instructors.length; i++){
+        for (var j = 0; j < instructors.length; j ++)
+        if (instructors[i].name.length > instructors[j].name.length && instructors[i].name.length > longestName.length){
+            longestName = instructors[i].name;
+            result = instructors[i];
+        } else if (instructors[j].name.length > instructors[i].name.length && instructors[j].name.length > longestName.length) {
+            longestName = instructors[j].name;
+            result = instructors[j]
+        }
+    }
+    return result;
+};
+
+console.log(instructorWithLongestName([
+    {name: "Samuel", course: "iOS"},
+    {name: "Jeremiah", course: "Web"},
+    {name: "Ophilia", course: "Web"},
+    {name: "Donald", course: "Web"}
+]));
+console.log(instructorWithLongestName([
+    {name: "Matthew", course: "Web"},
+    {name: "David", course: "iOS"},
+    {name: "Domascus", course: "Web"}
+]));
