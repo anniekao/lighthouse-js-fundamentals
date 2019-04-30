50736901d584de4e37f1357714bf0de14f235404
Annie <anniekao@posteo.net>
2019-04-30 21:23:25 +0200

    Added kata exercise change-calculator with a complete, yet buggy calculateChange function

---
+++ katas/kata10-change-calculator.js
@@ -0,0 +1,21 @@
+let calculateChange = function (total, cash) {
+    const MONEY = {twenty: 2000, ten: 1000, five: 500, twoDollars: 200, dollar: 100, quarter: 25, 
+                    dime: 10, nickel: 5, penny: 1};
+    
+    var change = {};
+    var remainder = (cash - total);
+
+    
+    for (var key in MONEY){
+        if (Math.floor(remainder/MONEY[key]) > 0){
+            change[key] = Math.floor(remainder / MONEY[key]);
+            remainder = remainder - MONEY[key];
+        } 
+    }
+    console.log(change);
+};
+
+// console.log(calculateChange(1787, 2000));
+console.log(calculateChange(2623, 4000));
+// console.log(calculateChange(501, 1000));
+
