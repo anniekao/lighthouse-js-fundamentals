632475c4a9a10e733c4581d2d354224e3472c1ac
Annie <anniekao@posteo.net>
2019-04-30 22:51:26 +0200

    Fixed the bug! The calculateChange function now works.

--- katas/kata10-change-calculator.js
+++ katas/kata10-change-calculator.js
@@ -9,13 +9,13 @@ let calculateChange = function (total, cash) {
     for (var key in MONEY){
         if (Math.floor(remainder/MONEY[key]) > 0){
             change[key] = Math.floor(remainder / MONEY[key]);
-            remainder = remainder - MONEY[key];
+            remainder = remainder - (MONEY[key] * Math.floor(remainder / MONEY[key]));
         } 
     }
-    console.log(change);
+   return change;
 };
 
-// console.log(calculateChange(1787, 2000));
+console.log(calculateChange(1787, 2000));
 console.log(calculateChange(2623, 4000));
-// console.log(calculateChange(501, 1000));
+console.log(calculateChange(501, 1000));
 
