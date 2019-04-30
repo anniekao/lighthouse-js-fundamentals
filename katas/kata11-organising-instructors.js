62ad18ad622469c3b4f1be6990800bad1bee2731
Annie <anniekao@posteo.net>
2019-04-30 22:54:06 +0200

    Added kata exercise organising-instructors. Solution to come.

---
+++ katas/kata11-organising-instructors.js
@@ -0,0 +1,37 @@
+const organizeInstructors = function (instructors) {
+    // Put your solution here
+};
+
+console.log(organizeInstructors([{
+                name: "Samuel",
+                course: "iOS"
+            },
+            {
+                name: "Victoria",
+                course: "Web"
+            },
+            {
+                name: "Karim",
+                course: "Web"
+            },
+            {
+                name: "Donald",
+                course: "Web"
+            }
+        ]); console.log(organizeInstructors([{
+                    name: "Brendan",
+                    course: "Blockchain"
+                },
+                {
+                    name: "David",
+                    course: "Web"
+                },
+                {
+                    name: "Martha",
+                    course: "iOS"
+                },
+                {
+                    name: "Carlos",
+                    course: "Web"
+                }
+            ]);
