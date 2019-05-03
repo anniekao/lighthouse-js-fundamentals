bda86a290c2c0c1c6973cbae0d2a5959c76ed625
Annie <anniekao@posteo.net>
2019-05-03 22:48:36 +0200

    Added createArray function

--- katas/kata15-queen-threat-detector.js
+++ katas/kata15-queen-threat-detector.js
@@ -2,3 +2,8 @@ const generateBoard = function(whiteQueen, blackqueen){
     var board = createArray(size, def);
 };
 
+// Takes in the size of the array/number of nested arrays and the default contents of each nested array
+// Returns a nested array
+function createArray(size, def) {
+
+}
