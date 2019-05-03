b7422a8d9c8c0fb1355f8bba0e12a0b12777e52d
Annie <anniekao@posteo.net>
2019-05-03 21:58:08 +0200

    Update function squareCode

--- katas/kata14-square-code.js
+++ katas/kata14-square-code.js
@@ -1,9 +1,19 @@
 const squareCode = function (message) {
     var rndSqRoot = Math.ceil(Math.sqrt(message.length));
-    var codedArr = createColumns(message, rndSqRoot);
-    var messageNoSpace = removeSpaces(message);
+    var messageNoSpaces = removeSpaces(message);
+    var codedArr = sqMessage(messageNoSpaces);
     
 };
+
+function sqMessage(message){
+    var codedResult = [];
+
+    for (var i = 0; i < message.length; i += rndSqRoot) {
+        codedResult.push(message.slice(i, i + rndSqRoot));
+    }
+
+    return codedResult;
+}
 
 function removeSpaces(message){
     var noSpaces = '';
@@ -17,16 +27,14 @@ function removeSpaces(message){
     return noSpaces;
 }
 
-console.log(removeSpaces('Hey there neighbour'));
+// function createColumns(message, rndSqRoot){
+//     var result = [];
 
-function createColumns(message, rndSqRoot){
-    var result = [];
-
-    for (var i = 0; i < rndSqRoot; i++){
-        result.push([]);
-    }
-    return result;
-}
+//     for (var i = 0; i < rndSqRoot; i++){
+//         result.push([]);
+//     }
+//     return result;
+// }
 
 console.log(squareCode("chill out"));
 // console.log(squareCode("feed the dog"));
