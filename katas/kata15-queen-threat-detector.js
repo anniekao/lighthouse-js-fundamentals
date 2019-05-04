506eb5a517a90d904efdc188bb2a42afd17454c8
Annie <anniekao@posteo.net>
2019-05-04 12:23:18 +0200

    Update threatDetector function

--- katas/kata15-queen-threat-detector.js
+++ katas/kata15-queen-threat-detector.js
@@ -1,18 +1,20 @@
 const generateBoard = function(whiteQueen, blackqueen){
-    var board = createArray(8);
+    var board = createArray;
     var queensOnBoard = placeQueens(board, whiteQueen, blackqueen);
 
-    return queensOnBoard;
+
+    console.log(queensOnBoard);
+    return threatDetector(queensOnBoard);
 };
 
 // Takes in the size of the array/number of nested arrays. The default content of the nested arrays are 0s
 // Returns a nested array
-function createArray(size) {
+function createArray() {
     var arr = [];
 
-    for (var i = 0; i < size; i++){
+    for (var i = 0; i < 8; i++){
         arr.push([]);
-        for (var j = 0; j < size; j++){
+        for (var j = 0; j < 8; j++){
             arr[i].push(0);
         }
     }
@@ -35,6 +37,26 @@ function placeQueens(board, pos1, pos2) {
     return board;
 }
 
-var whiteQueen = [0,5];
-var blackQueen = [5,0];
-console.log(generateBoard(whiteQueen, blackQueen));
+// Takes in a 2D array that represents a board and checks if the two queens can attack each other
+// Returns a boolean
+function threatDetector(board){
+    
+    // Checks horizontally for other queens
+    for (var i = 0; i < board.length; i++){
+        for (var j = 0; j < board.length; j++){
+            if (board[i][i] == board[i][j]){
+                return true;
+            }
+        }
+    } return false;
+}
+
+var whiteQueen = [5,5];
+var blackQueen = [5,7];
+var board = placeQueens(createArray(), whiteQueen, blackQueen);
+
+console.log(threatDetector(board));
+
+// var whiteQueen = [0,5];
+// var blackQueen = [5,0];
+// console.log(generateBoard(whiteQueen, blackQueen));
