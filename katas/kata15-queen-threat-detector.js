1cccfc39398a42961d71b2324c2abbbc99d37115
Annie <anniekao@posteo.net>
2019-05-04 10:55:43 +0200

    Update placeQueens function

--- katas/kata15-queen-threat-detector.js
+++ katas/kata15-queen-threat-detector.js
@@ -1,5 +1,8 @@
 const generateBoard = function(whiteQueen, blackqueen){
-    var board = createArray(size);
+    var board = createArray(8);
+    var queensOnBoard = placeQueens(board, whiteQueen, blackqueen);
+
+    return queensOnBoard;
 };
 
 // Takes in the size of the array/number of nested arrays. The default content of the nested arrays are 0s
@@ -13,14 +16,25 @@ function createArray(size) {
             arr[i].push(0);
         }
     }
-
     return arr;
 }
 
 
 // Takes in the positions of the white and black queen as arrays
 // Updates the board, so that these positions change to 1's
-function placeQueens(pos1, pos2) {
+function placeQueens(board, pos1, pos2) {
+    pos1x = pos1[0];
+    pos1y = pos1[1];
 
+    pos2x = pos2[0];
+    pos2y = pos2[1];
+
+    board[pos1x][pos1y] = 1;
+    board[pos2x][pos2y] = 1;
+
+    return board;
 }
-console.log(createArray(8, '0'));
+
+var whiteQueen = [0,5];
+var blackQueen = [5,0];
+console.log(generateBoard(whiteQueen, blackQueen));
