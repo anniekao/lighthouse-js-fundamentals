2b078a75a6965a0e4c3d924892176dab909efad2
Annie <anniekao@posteo.net>
2019-05-14 21:28:32 +0200

    Update -- now with an animated bar!

--- stretch-project/main.js
+++ stretch-project/main.js
@@ -1,1 +1,15 @@
-$('#graph-title').text('Stretch Project: Bar Graph')
+$('#graph-title').text('Stretch Project: Bar Graph');
+
+var bar = document.createElement('div');
+bar.className = 'graph-bar';
+var text = document.createTextNode('');
+bar.appendChild(text);
+document.getElementById('content').appendChild(bar);
+
+$('.graph-bar').animate({
+    width: '500px'
+}, 1500);
+
+function drawBarChart(data, options, element){
+    
+}
