49efee29c977b5504795dae46075779e6db0aa7b
Annie <anniekao@posteo.net>
2019-05-14 22:15:29 +0200

    Update -- drawBar function

--- stretch-project/main.js
+++ stretch-project/main.js
@@ -1,15 +1,27 @@
-$('#graph-title').text('Stretch Project: Bar Graph');
+$('#graph-title').text('Blah Blah Bar Graph');
 
-var bar = document.createElement('div');
-bar.className = 'graph-bar';
-var text = document.createTextNode('');
-bar.appendChild(text);
-document.getElementById('content').appendChild(bar);
+function drawBar(num){
+    for (var i = 0; i < num; i++){
+        var bar = document.createElement('div');
+        bar.className = 'graph-bar';
+        var text = document.createTextNode('');
+        bar.appendChild(text);
+    
+        var empty = document.createElement('div');
+        empty.className = 'empty-bar';
+        empty.appendChild(text);
+        
+        document.getElementById('content').appendChild(bar);
+        document.getElementById('content').appendChild(empty);
 
-$('.graph-bar').animate({
-    width: '500px'
-}, 1500);
+        $('.graph-bar').animate({
+            width: '500px'
+        }, 1500);
+    }
+}
 
 function drawBarChart(data, options, element){
     
 }
+
+drawBar(3);
