e70bfed2204d01bc981d9ee2aab135bbe6bf52e1
Annie <anniekao@posteo.net>
2019-05-18 17:25:33 +0200

    Update -- more readme info and createGraphBars function

--- stretch-project/README.md
+++ stretch-project/README.md
@@ -1,5 +1,12 @@
 # Notes
+Started off by trying to dynamically create div elements that would represent each bar on the graph. The problem was how I would target each bar div in order to change it's color and the position of the text within it depending on the wants of the user.
 
 May 18/2019 -- Decided to scrap the idea of creating the bar graph using divs after finding the following tutorial
 about using the HTML <svg>/Scalable Vector Graphic element to create rectangles with JavaScript in order to achieve
 the same effect.
+
+# What I've Learned/Practiced By Doing This Project
+
+- SVG elements
+- JavaScript: setting element attributes, style, appending elements
+- Jquery
--- stretch-project/main.js
+++ stretch-project/main.js
@@ -10,13 +10,31 @@ function setSVGWidthHeight(width, height){
     svg.setAttribute('height', height);
 }
 
+// Create svg rect elements to represent the bars of the graph depending on the inputted number
+function createGraphBars(dataSet){
+    var svg = document.getElementById('graph-body');
+    var height = svg.getAttribute('height');
+    var width = svg.getAttribute('width');
+    var barWidth = width/dataSet.length;
+    var barPadding = 5;
 
+    for (var i = 0; i < dataSet.length; i++){
+        var rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+        rect.setAttribute('y', height - dataSet[i]);
+        rect.setAttribute('height', dataSet[i]);
+        rect.setAttribute('width', barWidth - barPadding);
+        rect.style.fill = 'black';
+        var translate = [barWidth * i, 0];
+        rect.setAttribute('transform', "translate(" + translate + ")");
 
+        svg.appendChild(rect);
 
+    }
 
-
+}
 
 var dataSet = [20, 30, 40, 50, 60, 120];
 graphTitle('Blah Blah Bar Graph');
 setSVGWidthHeight(500, 300);
+createGraphBars(dataSet);
 
