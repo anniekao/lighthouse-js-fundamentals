bbac1d8c725b92cbbd7c348758fb2602eaccb32c
Annie <anniekao@posteo.net>
2019-05-18 13:35:05 +0200

    Update

--- stretch-project/main.js
+++ stretch-project/main.js
@@ -1,3 +1,22 @@
+// Change graph title
 function graphTitle(title){
     $('#graph-title').text(title);
 }
+
+// Set the width and height of the svg element
+function setSVGWidthHeight(width, height){
+    var svg = document.getElementById('graph-body');
+    svg.setAttribute('width', width);
+    svg.setAttribute('height', height);
+}
+
+
+
+
+
+
+
+var dataSet = [20, 30, 40, 50, 60, 120];
+graphTitle('Blah Blah Bar Graph');
+setSVGWidthHeight(500, 300);
+
