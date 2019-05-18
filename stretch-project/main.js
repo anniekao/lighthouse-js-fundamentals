92c5e7492e1b5e0d2f3ad5d84c30d6f0037b4d00
Annie <anniekao@posteo.net>
2019-05-18 12:51:08 +0200

    Update -- starting from scratch

--- stretch-project/main.js
+++ stretch-project/main.js
@@ -1,27 +1,3 @@
-$('#graph-title').text('Blah Blah Bar Graph');
-
-function drawBar(num){
-    for (var i = 0; i < num; i++){
-        var bar = document.createElement('div');
-        bar.className = 'graph-bar';
-        var text = document.createTextNode('');
-        bar.appendChild(text);
-    
-        var empty = document.createElement('div');
-        empty.className = 'empty-bar';
-        empty.appendChild(text);
-        
-        document.getElementById('content').appendChild(bar);
-        document.getElementById('content').appendChild(empty);
-
-        $('.graph-bar').animate({
-            height: '100px'
-        }, 1500);
-    }
+function graphTitle(title){
+    $('#graph-title').text(title);
 }
-
-function drawBarChart(data, options, element){
-    
-}
-
-drawBar(1);
