4b771949c4a0ea1b1f49842c07bc3cc687a9677d
Annie <anniekao@posteo.net>
2019-05-18 17:54:15 +0200

    Update -- added drawBars function

--- stretch-project/main.js
+++ stretch-project/main.js
@@ -10,18 +10,14 @@ function setSVGWidthHeight(width, height){
     svg.setAttribute('height', height);
 }
 
-// Given an array of data, an object containing the width and height of the chart and the element where the chart appears
-// a bar chart is created.
-function drawBarChart(data, options, element){
-    setSVGWidthHeight(options.width, options.height);
-
+function drawBars(data, element){
     var svg = document.getElementById(element);
     var height = svg.getAttribute('height');
     var width = svg.getAttribute('width');
-    var barWidth = width/data.length;
+    var barWidth = width / data.length;
     var barPadding = 5;
 
-    for (var i = 0; i < data.length; i++){
+    for (var i = 0; i < data.length; i++) {
         var rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
         rect.setAttribute('y', height - data[i]);
         rect.setAttribute('height', data[i]);
@@ -31,13 +27,18 @@ function drawBarChart(data, options, element){
         rect.setAttribute('transform', "translate(" + translate + ")");
 
         svg.appendChild(rect);
-
     }
-
 }
 
-var dataSet = [20, 30, 40, 50, 60, 120];
-var options = {width: 500, height: 300};
+// Given an array of data, an object containing the width and height of the chart and the element where the chart appears
+// a bar chart is created.
+function drawBarChart(data, options, element){
+    setSVGWidthHeight(options.width, options.height);
+    drawBars(data, element);   
+}
+
+var dataSet = [20, 30, 40, 50, 60, 120, 80];
+var options = {width: 800, height: 300};
 var element = 'graph-body';
 
 graphTitle('Blah Blah Bar Graph');
